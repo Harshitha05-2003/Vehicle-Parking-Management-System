db5f28b37e0939c9e26df618a0701a0d464a64f1
Harshitha05-2003 <harshithadalavai@gmail.com>
2026-02-23 00:06:02 +0530

    Initial commit: Add Vehicle Parking Management System

---
+++ VPMS/slot-service/src/test/java/com/parking/slot_service/SlotServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.parking.slot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test verifies that the Spring application context loads successfully
+		// If there are any configuration issues, this test will fail
+	}
+}
